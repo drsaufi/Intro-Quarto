5067396801669ecc57e93a4e41fa96d6656ad9aa
drsaufi <145425256+drsaufi@users.noreply.github.com>
2024-10-24 23:44:40 +0800

    Add files

---
+++ Penguins.R
@@ -0,0 +1,86 @@
+
+# Setting Up the Environment ----------------------------------------------
+
+library(palmerpenguins) # To use dataset for analysis.
+library(tidyverse) # For data manipulation and visualization.
+library(DT) # To create interactive data tables.
+library(ggplot2) # For creating customizable visualizations.
+
+# Summary of Dataset ------------------------------------------------------
+
+data("penguins")
+summary(penguins)
+
+# Descriptive Tables ------------------------------------------------------
+
+datatable(penguins, filter = "top")
+
+# Data Visualization ------------------------------------------------------
+
+## Penguin Mass vs. Flipper Length
+
+mass_flipper <- ggplot(data = penguins, 
+                       aes(x = flipper_length_mm,
+                           y = body_mass_g)) +
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 3,
+             alpha = 0.8) +
+  scale_color_manual(values = c("darkorange","purple","cyan4")) +
+  labs(title = "Penguin size, Palmer Station LTER",
+       subtitle = "Flipper length and body mass for Adelie, Chinstrap and Gentoo Penguins",
+       x = "Flipper length (mm)",
+       y = "Body mass (g)",
+       color = "Penguin species",
+       shape = "Penguin species") +
+  theme(legend.position = c(0.2, 0.7),
+        plot.title.position = "plot",
+        plot.caption = element_text(hjust = 0, face= "italic"),
+        plot.caption.position = "plot")
+mass_flipper
+
+## Flipper Length vs. Bill Length
+
+flipper_bill <- ggplot(data = penguins,
+                       aes(x = flipper_length_mm,
+                           y = bill_length_mm)) +
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 3,
+             alpha = 0.8) +
+  scale_color_manual(values = c("darkorange","purple","cyan4")) +
+  labs(title = "Flipper and bill length",
+       subtitle = "Dimensions for Adelie, Chinstrap and Gentoo Penguins at Palmer Station LTER",
+       x = "Flipper length (mm)",
+       y = "Bill length (mm)",
+       color = "Penguin species",
+       shape = "Penguin species") +
+  theme(legend.position = c(0.85, 0.15),
+        plot.title.position = "plot",
+        plot.caption = element_text(hjust = 0, face= "italic"),
+        plot.caption.position = "plot")
+flipper_bill
+
+## Bill Length vs. Bill Depth
+
+bill_len_dep <- ggplot(data = penguins,
+                       aes(x = bill_length_mm,
+                           y = bill_depth_mm,
+                           group = species)) +
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 3,
+             alpha = 0.8) +
+  geom_smooth(method = "lm", se = FALSE, aes(color = species)) +
+  scale_color_manual(values = c("darkorange","purple","cyan4")) +
+  labs(title = "Penguin bill dimensions",
+       subtitle = "Bill length and depth for Adelie, Chinstrap and Gentoo Penguins at Palmer Station LTER",
+       x = "Bill length (mm)",
+       y = "Bill depth (mm)",
+       color = "Penguin species",
+       shape = "Penguin species") +
+  theme(legend.position = c(0.85, 0.15),
+        plot.title.position = "plot",
+        plot.caption = element_text(hjust = 0, face= "italic"),
+        plot.caption.position = "plot")
+bill_len_dep
